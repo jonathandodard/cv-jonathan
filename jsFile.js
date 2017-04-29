67bf35c48f2b24064012bc2c22ecdcb594acb83b
mikijoe <jonathan.dodard@gmail.com>
2017-04-29 22:59:27 +0200

    update cv index,html js css ans image

---
+++ jsFile.js
@@ -0,0 +1,9 @@
+/**
+ * Created by jonathan on 4/27/17.
+ */
+// $('.krealinks div').mouseout(function () {
+//     $('.krealinks div').css("display", "block")
+// });
+// $('.krealinks div').mouseover(function () {
+//     $('.krealinks div').css("display", "none")
+// });
